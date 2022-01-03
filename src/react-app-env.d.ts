bfff41953f18ca0d8801283983c3e434ab68ffdd
mohammad-hosein-shahpouri <mh.shahpouri@gmail.com>
2022-01-03 18:50:24 +0000

    Deploying to gh-pages from @ mohammad-hosein-shahpouri/Resume@fa7bd45db5fc7adf81261cc5fffbf6d1fa983043 🚀

--- src/react-app-env.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.woff';
-declare module '*.ttf';
-declare module '*.svg';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.mp3';
-/// <reference types="react-scripts" />
